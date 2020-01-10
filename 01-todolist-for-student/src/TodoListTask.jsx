ef9514c5f183a4438a1d560ed37a3d51deecdec1
papavmode <papavmode@gmail.com>
2020-01-10 10:58:42 +0300

    papavmode

---
+++ 01-todolist-for-student/src/TodoListTask.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class TodoListTask extends React.Component {
+  render() {
+    return (
+      <div className="todoList-tasks">
+          <div className="todoList-task">
+              <input type="checkbox" checked={true}/>
+              <span>CSS</span>
+          </div>
+          <div className="todoList-task">
+              <input type="checkbox" checked={false}/>
+              <span>JS</span>
+          </div>
+          <div className="todoList-task">
+              <input type="checkbox" checked={false}/>
+              <span>ReactJS</span>
+          </div>
+          <div className="todoList-task">
+              <input type="checkbox" checked={true}/>
+              <span>Patterns</span>
+          </div>
+      </div>                    
+    );
+  }
+}
+
+export default TodoListTask;
